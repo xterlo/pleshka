bd30fe40a46c52cb91e53527e2220f0529d18629
xterlo <36822079+xterlo@users.noreply.github.com>
2019-07-31 19:18:17 +0300

    add

---
+++ pleshka.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+from openpyxl import load_workbook
+import requests
+print("Download...")
+check = requests.get("https://mpt.ru/priyemnaya-komissiya/09.02.07%20бюджет%209%20кл..xlsx")
+f = open("1.xlsx",'wb')
+f.write(check.content)
+f.close
+print("Opening file...")
+wb = load_workbook('1.xlsx')
+numb=6
+name=''
+while not name == "Шорин Евгений Максимович":
+    name = wb.active['C'+str(numb)].value
+    ball = wb.active['D'+str(numb)].value
+    print(str(numb-5)+') '+name+" ["+str(ball)+"]")
+    numb+=1
+input()
